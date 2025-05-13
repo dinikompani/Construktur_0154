960e4e7fa0ef978c561ef547f373f0ca1711946d
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-05-13 10:23:14 +0700

    Implementasi constructor di class mahasiswa

---
+++ constructor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+    mahasiswa(); //constructor
+};
+
+mahasiswa::mahasiswa(){
+    cout << "Constructor Terpanggil" <<endl; // Definisi constructor
+}
+
